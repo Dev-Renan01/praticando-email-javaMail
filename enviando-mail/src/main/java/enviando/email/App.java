76949f838fa096ad236aab5b1fd78a19ecade1b5
Thiago Renan <thiagorenan1108@gmail.com>
2025-06-22 23:48:05 -0300

    Configurando as propriedades de conexão

--- enviando-mail/src/main/java/enviando/email/App.java
+++ enviando-mail/src/main/java/enviando/email/App.java
@@ -2,10 +2,33 @@ package enviando.email;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Properties;
+
 public class App {
 
     @Test
     public void testeEmail(){
+        /*SMTP - protocolo padrão para envio de E-mail*/
+
+        Properties properties = new Properties();
+        /*autorização*/
+        properties.put("mail.smtp.auth", "true");// Hash Map, contém chave e valor / key, value
+
+        /*Autenticação*/
+        properties.put("mail.smtp.starttls", "true"); // Garantiar a segurança da conexão entre a aplicação
+
+        /*Servidor Gmail Google*/
+        properties.put("mail.smtp.host.", "smtp.gmail.com");//Enviar os e-mails através do servidor de envio de e-mails do Gmail.
+
+        /*Porta do servidor*/
+        properties.put("mail.smtp.port", "465");//Caminho do servidor / A porta de rede usada para se conectar ao servidor de envio de e-mails (SMTP).
+
+        /*Especifica se a porta a ser conectada pelo socket*/
+        properties.put("mail.smtp.socketFactory.port", "465");// Define a porta 465 para conexões SMTP
+
+        /*Clase socket de conecão ao SMTP*/
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        
 
     }
 
